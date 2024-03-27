b88576828aaec0d7bc0f36bf00807645e696dee2
JehSA <jef.almeida@gmail.com>
2024-03-27 20:44:54 -0300

    Inicio do CRUD (POST favorite e user / DELETE user).

--- schemas.py
+++ schemas.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel
 class UserCreateInput(BaseModel):
     name: str
 
+class UserFavoriteAddInput(BaseModel):
+    user_id: int
+    symbol: str
+
 class StandardOutput(BaseModel):
     message: str
 
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from services import UserService
-from schemas import UserCreateInput, StandardOutput, ErrorOutput
+from services import UserService, FavoriteService
+from schemas import (
+    UserCreateInput, StandardOutput, ErrorOutput, UserFavoriteAddInput                     
+)
 
 
 user_router = APIRouter(prefix='/user')
@@ -11,6 +13,24 @@ assets_router = APIRouter(prefix='/assets')
 async def user_create(user_input: UserCreateInput):
     try:
         await UserService.create_user(name=user_input.name)
+        return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
+    
+    
+@user_router.delete('/delete/{user_id}', response_model=StandardOutput, responses={400: {'model':ErrorOutput}})
+async def user_delete(user_id: int):
+    try:
+        await UserService.delete_user(user_id)
+        return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
+    
+    
+@user_router.post('/favorite/add', response_model=StandardOutput, responses={400: {'model':ErrorOutput}})
+async def user_favorite_add(favorite_add: UserFavoriteAddInput):
+    try:
+        await FavoriteService.add_favorite(user_id=favorite_add.user_id, symbol=favorite_add.symbol)
         return StandardOutput(message='OK')
     except Exception as error:
         raise HTTPException(400, detail=str(error))
